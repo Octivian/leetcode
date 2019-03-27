0c783cecfa0e125cf7e4b04791421606fd507d5a
lixia <lixiangizzue@hotmail.com>
2019-03-27 23:46:15 +0800

    O(m+n) solution

--- src/MedianOfTwoSortedArrays.java
+++ src/MedianOfTwoSortedArrays.java
@@ -1,17 +1,43 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class MedianOfTwoSortedArrays {
-    class Solution {
-        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            int[] left, right;
 
-            int[] leftNums1 = Arrays.copyOfRange(nums1, 0, nums1.length / 2 + 1);
-            int[] leftNums2 = Arrays.copyOfRange(nums2, 0, nums2.length / 2 + 1);
+    /**
+     * O(m+n)
+     */
+    class Solution1 {
+        public  double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-            int[] rightNums1 = Arrays.copyOfRange(nums1, nums1.length / 2 + 1, nums1.length);
-            int[] rightNums2 = Arrays.copyOfRange(nums2, nums1.length / 2 + 1, nums2.length);
 
-            return 0d;
+            int i=0;
+            int j=0;
+
+            int length = nums1.length+nums2.length;
+            int[] allNums = new int[length];
+
+            int x=0;
+
+            while(i<nums1.length && j<nums2.length){
+
+
+                if (nums1[i]<=nums2[j]){
+                    allNums[x++]=nums1[i++];
+                }else{
+                    allNums[x++]=nums2[j++];
+                }
+            }
+
+            while(i<nums1.length){
+                allNums[x++]=nums1[i++];
+            }
+            while(j<nums2.length){
+                allNums[x++]=nums2[j++];
+            }
+
+
+            return length%2==0?(allNums[length/2]+allNums[length/2-1])/2d:allNums[length/2];
         }
     }
 }
