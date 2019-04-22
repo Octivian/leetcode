5f79ac1fd9ec9cf4c5a499ef7cc9fa87f8990a85
lixiang <lixiang_sed@smartisan.com>
2019-04-22 15:46:11 +0800

    stack for TargetSum

---
+++ src/stack/TargetSum.java
@@ -0,0 +1,21 @@
+package stack;
+
+public class TargetSum {
+    class Solution {
+        public int findTargetSumWays(int[] nums, int S) {
+            return DFS(nums, 0, 0, S);
+        }
+
+        private int DFS(int[] nums, int index, int cur, int S) {
+            if (index == nums.length) {
+                if (cur == S) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+
+            return DFS(nums, index + 1, cur + nums[index], S) + DFS(nums, index + 1, cur - nums[index], S);
+        }
+    }
+}
