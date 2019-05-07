f8506e6b5d40b4b070a2890081fd4e9fc9503c53
SecondTheWorld <lixiangizzue@hotmail.com>
2019-05-07 23:00:30 +0800

    Have fucked Diagonal Traverse

--- src/arrayAndString/medium/DiagonalTraverse.java
+++ src/arrayAndString/medium/DiagonalTraverse.java
@@ -10,18 +10,24 @@ public class DiagonalTraverse {
             }
             int n = matrix[0].length;
             int[] ints = new int[m * n];
-            int code = 0;
-            boolean up = true;
             int index = 0;
-            int x = 0;
-            while (code <= ((m - 1) + (n - 1))) {
+            boolean up = false;
+
+            //every loop is a slash direction , x + y = code , code start from 0 to (m-1)+(n-1)
+            for (int code = 0, x = 0; code <= ((m - 1) + (n - 1)); code++) {
+                //determine every slash loop's start x index
                 int i = x;
-                for (; i >= 0 && code - i < n; ) {
+                // 2 situiations , up and down, up or down make sure not out of boundry m and n;
+                while ((!up && i >= 0 && code - i < n) || (up && i < m && code - i >= 0)) {
                     ints[index++] = matrix[i][code - i];
+                    //up means to right and up
+                    //down means to left and down
                     i = up ? i + 1 : i - 1;
                 }
-                x = up ? Math.min(i, m - 1) : Math.max(i, 0);
-                code++;
+
+                x = up ? Math.min(i, m - 1) : (code - i < n) ? 0 : i + 2;
+                up = up ? false : true;
+
             }
 
             return ints;
