91c2b5a8c92aaedd52e850dc0f387ea12da6d33f
lixiang <lixiang_sed@smartisan.com>
2019-05-24 14:34:16 +0800

    finish classic linkedlist question

--- src/linkedList/classic/easy/PalindromeLinkedList.java
+++ src/linkedList/classic/easy/PalindromeLinkedList.java
@@ -10,18 +10,39 @@ public class PalindromeLinkedList {
         }
     }
 
-    //    class Solution {
-    //        public boolean isPalindrome(ListNode head) {
-    //            ListNode s = head;
-    //            ListNode f = head;
-    //            ListNode left = null;
-    //            while (f != null && f.next != null && f.next.next != null) {
-    //                f = f.next.next;
-    //                ListNode tmp = s.next;
-    //                s.next = left;
-    //                left = s;
-    //                s = tmp;
-    //            }
-    //        }
-    //    }
+    class Solution {
+        /**
+         * 翻转前半段linkedList 直到中点
+         */
+        public boolean isPalindrome(ListNode head) {
+            ListNode s = head;
+            ListNode f = head;
+            ListNode left = null;
+            while (f != null && f.next != null) {
+                f = f.next.next;
+                ListNode tmp = s.next;
+                s.next = left;
+                left = s;
+                s = tmp;
+            }
+
+            /**
+             * 奇数个节点情况
+             */
+            if (f != null && f.next == null) {
+                ListNode mid = new ListNode(s.val);
+                mid.next = left;
+                left = mid;
+            }
+
+            while (s != null && left != null) {
+                if (s.val != left.val) {
+                    return false;
+                }
+                s = s.next;
+                left = left.next;
+            }
+            return true;
+        }
+    }
 }
