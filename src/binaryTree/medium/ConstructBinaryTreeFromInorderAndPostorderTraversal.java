08c753486c16d6c3c2fd23be00de878c86e44c19
lixia <lixiangizzue@hotmail.com>
2019-06-11 19:43:13 +0800

    construct binarytree by inorder and postorder

--- src/binaryTree/medium/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/binaryTree/medium/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -11,29 +11,29 @@ public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
         }
     }
 
-    /*
-    Too hard
-
-    todo
-     */
     class Solution {
-        int[] iArray;
-        int[] pArray;
 
         public TreeNode buildTree(int[] inorder, int[] postorder) {
-            int size = inorder.length;
-            iArray = inorder;
-            pArray = postorder;
-            if (size == 0) {
-                return null;
-            }
-            TreeNode root = new TreeNode(postorder[size - 1]);
-            TreeNode tmp = root;
-            return root;
+            return build(inorder, postorder, postorder.length - 1, 0, inorder.length - 1);
         }
 
-        public void build(TreeNode root) {
-
+        public TreeNode build(int[] inorder, int[] postorder, int ppos, int ins, int ine) {
+            if (ins > ine) {
+                return null;
+            }
+            TreeNode node = new TreeNode(postorder[ppos]);
+            int mid = 0;
+            for (int i = 0; i < inorder.length; i++) {
+                if (inorder[i] == postorder[ppos]) {
+                    mid = i;
+                }
+            }
+            int rightNodeIndex = ppos - 1; // postorder
+            int rightNodeNums = ine - mid; //inorder
+            int leftNodeIndex = rightNodeIndex - rightNodeNums;
+            node.left = build(inorder, postorder, leftNodeIndex, ins, mid - 1);
+            node.right = build(inorder, postorder, rightNodeIndex, mid + 1, ine);
+            return node;
         }
     }
 }
